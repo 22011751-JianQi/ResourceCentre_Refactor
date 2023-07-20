9fa1b8e8412520b3533e036d8b18279bea0297c5
22015248 <22015248@LAPTOP-17AD0DSF>
2023-07-20 12:11:55 +0800

    Merge branch 'master' of https://github.com/22011751-JianQi/ResourceCentre_Refactor.git

--- ResourceCentre_Refactor/src/ResourceCentre.java
+++ ResourceCentre_Refactor/src/ResourceCentre.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class ResourceCentre {
-	//Hamzah
+	//Hamzah - Extract Constant
 	private static final int ITEM_TYPE_CHROMEBOOK = 2;
 	private static final int ITEM_TYPE_CAMCORDER = 1;
 	private static final int OPTION_RETURN = 4;
